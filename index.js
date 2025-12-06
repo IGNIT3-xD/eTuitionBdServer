3de3505fbfda3e95fdb3fc60d888337f49ed4f49
IGNIT3-xD <md.imranali089@gmail.com>
2025-12-06 20:35:41 +0600

    Server setup

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(express.json())
+app.use(cors())
+
+const uri = process.env.URI;
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+
+        // Collections
+        const db = client.db('eTuitionBd');
+        const allTuitions = db.collection('all_tuitions');
+        const allTutors = db.collection('all_tutors')
+
+        // Roues
+        // Get all tuitions
+        try {
+            app.get('/tuitions', async (req, res) => {
+                const result = await allTuitions.find().toArray()
+                res.send(result)
+            })
+        }
+        catch {
+            res.status(500).send({ message: 'Failed to fetch tuitions data' });
+        }
+
+        // Get all tutors
+        try {
+            app.get('/tutors', async (req, res) => {
+                const result = await allTutors.find().toArray()
+                res.send(result)
+            })
+        }
+        catch {
+            res.status(500).send({ message: 'Failed to fetch tutors data' });
+        }
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Server is running')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
