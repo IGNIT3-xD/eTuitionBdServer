950695e98ef567018df80554e6d36f63adc584fb
IGNIT3-xD <md.imranali089@gmail.com>
2025-12-11 12:07:13 +0600

    Update tuition api added & try catch fixed

--- index.js
+++ index.js
@@ -30,79 +30,137 @@ async function run() {
 
         // -----Routes----- //
 
+        // ---Tuitions Releted Apis--- //
+
         // Get all tuitions
-        try {
-            app.get('/tuitions', async (req, res) => {
+        app.get('/tuitions', async (req, res) => {
+            try {
                 const result = await allTuitions.find().project({ schedule: 0, startDate: 0, postedBy: 0 }).toArray()
                 res.send(result)
-            })
-        }
-        catch {
-            res.status(500).send({ message: 'Failed to fetch tuitions data' });
-        }
+            }
+            catch {
+                res.status(500).send({ message: 'Failed to fetch tuitions data' });
+            }
+        })
+
+        // Get tuitions status 
+        app.get('/tuitions/approved', async (req, res) => {
+            try {
+                const result = await allTuitions.find({ status: 'Approved' }).toArray()
+                res.send(result)
+            }
+            catch {
+                res.status(500).send({ message: 'Failed to get tuitions status data' });
+            }
+        })
+
+        app.get('/tuitions/pending', async (req, res) => {
+            try {
+                const result = await allTuitions.find({ status: 'Pending' }).toArray()
+                res.send(result)
+            }
+            catch {
+                res.status(500).send({ message: 'Failed to get tuitions status data' });
+            }
+        })
 
         // Post tuition
-        try {
-            app.post('/tuitions', async (req, res) => {
+        app.post('/tuitions', async (req, res) => {
+            try {
                 const tuition = req.body
                 const result = await allTuitions.insertOne(tuition)
                 res.send(result)
-            })
-        }
-        catch {
-            res.status(500).send({ message: 'Failed to post tuitions data' });
-        }
+            }
+            catch {
+                res.status(500).send({ message: 'Failed to post tuitions data' });
+            }
+        })
 
         // Get tuition by email
-        try {
-            app.get('/my-tuitions', async (req, res) => {
+        app.get('/my-tuitions', async (req, res) => {
+            try {
                 const { email } = req.query
                 const result = await allTuitions.find({ "postedBy.email": email }).toArray()
                 res.send(result)
-            })
-        }
-        catch {
-            res.status(500).send({ message: 'Failed to get tuitions data' });
-        }
+            }
+            catch {
+                res.status(500).send({ message: 'Failed to get tuitions data' });
+            }
+        })
+
+        // Delete tuition
+        app.delete('/tuitions/:id', async (req, res) => {
+            try {
+                const { id } = req.params
+                const result = await allTuitions.deleteOne({ _id: new ObjectId(id) })
+                res.send(result)
+            }
+            catch {
+                res.status(500).send({ message: 'Failed to delete tuition data' });
+            }
+        })
+
+        // Update tuition
+        app.patch('/tuitions/:id', async (req, res) => {
+            try {
+                const id = req.params.id
+                const data = req.body
+                // console.log(id);
+                // console.log(data);
+
+                const query = { _id: new ObjectId(id) }
+                // console.log(req.body);
+                const updatedData = {
+                    $set: data
+                }
+                const result = await allTuitions.updateOne(query, updatedData)
+                res.send(result)
+            }
+            catch {
+                res.status(500).send({ message: 'Failed to update tuition data' });
+            }
+        })
 
         // Tuition details
-        try {
-            app.get('/tuitions/:id', async (req, res) => {
+        app.get('/tuitions/:id', async (req, res) => {
+            try {
                 const { id } = req.params
                 const result = await allTuitions.findOne({ _id: new ObjectId(id) })
                 res.send(result)
-            })
-        }
-        catch {
-            res.status(500).send({ message: 'Failed to fetch tuitions details data' })
-        }
+            }
+            catch {
+                res.status(500).send({ message: 'Failed to fetch tuitions details data' })
+            }
+        })
+
+        // ---Tutors Releted Apis--- //
 
         // Get all tutors
-        try {
-            app.get('/tutors', async (req, res) => {
+        app.get('/tutors', async (req, res) => {
+            try {
                 const result = await allTutors.find().project({ email: 0, about: 0, education: 0 }).toArray()
                 res.send(result)
-            })
-        }
-        catch {
-            res.status(500).send({ message: 'Failed to fetch tutors data' });
-        }
+            }
+            catch {
+                res.status(500).send({ message: 'Failed to fetch tutors data' });
+            }
+        })
 
         // Tutor details
-        try {
-            app.get('/tutors/:id', async (req, res) => {
+        app.get('/tutors/:id', async (req, res) => {
+            try {
                 const { id } = req.params
                 const result = await allTutors.findOne({ _id: new ObjectId(id) })
                 res.send(result)
-            })
-        }
-        catch {
-            res.status(500).send({ message: 'Failed to fetch tutor details' })
-        }
+            }
+            catch {
+                res.status(500).send({ message: 'Failed to fetch tutor details' })
+            }
+        })
 
         // Post user details
-        try {
-            app.post('/users', async (req, res) => {
+        app.post('/users', async (req, res) => {
+            try {
                 const user = req.body
                 // console.log(user);
 
@@ -115,11 +173,11 @@ async function run() {
 
                 const result = await allUsers.insertOne(user)
                 res.send(result)
-            })
-        }
-        catch {
-            res.status(500).send({ message: 'Failed to post role data' })
-        }
+            }
+            catch {
+                res.status(500).send({ message: 'Failed to post role data' })
+            }
+        })
 
         // Get user by their email
         try {
@@ -133,9 +191,11 @@ async function run() {
             res.status(500).send({ message: 'Failed to get user details' })
         }
 
+        // ---User Releted Apis--- //
+
         // Update user info
-        try {
-            app.patch('/users/:id', async (req, res) => {
+        app.patch('/users/:id', async (req, res) => {
+            try {
                 const { id } = req.params
                 const { name } = req.body
                 const query = { _id: new ObjectId(id) }
@@ -146,11 +206,11 @@ async function run() {
                 }
                 const result = await allUsers.updateOne(query, update)
                 res.send(result)
-            })
-        }
-        catch {
-            res.status(500).send({ message: 'Failed to update user info' })
-        }
+            }
+            catch {
+                res.status(500).send({ message: 'Failed to update user info' })
+            }
+        })
 
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
